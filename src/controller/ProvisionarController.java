1da54fc4e7d23b0518b638b5fc57fb6190d177c9
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-09-30 22:38:56 -0300

    Feito esqueleto do método que calculará a correção monetária.

---
+++ src/controller/ProvisionarController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import model.Query;
+
+public class ProvisionarController {
+
+	/**
+	 * Método que calcula a correção monetária de um valor qualquer.
+	 * 
+	 * @param query o conjunto de dados necessários para o cálculo dos juros.
+	 * @return o valor corrigido monetariamente.
+	 */
+	public static double calculaCorrecao (Query query) {
+		return 0;
+	}
+}
